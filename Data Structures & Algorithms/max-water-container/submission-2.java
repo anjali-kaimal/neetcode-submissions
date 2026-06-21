b6034da8f463d92bd6450496c8cba5e320c17779
Anjali B Kaimal <32356464+anjali-kaimal@users.noreply.github.com>
2026-06-21 16:37:08 +0530

    Add: max-water-container - submission-2

---
+++ Data Structures & Algorithms/max-water-container/submission-2.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxArea(int[] heights) {
+     int pointer1=0, pointer2=heights.length-1, height=0, width=0, maxArea=0;
+     while(pointer1<pointer2){
+        width=pointer2-pointer1;
+        if(heights[pointer1]>heights[pointer2]){
+            height=heights[pointer2];
+            pointer2--;
+        }
+        else{
+            height=heights[pointer1];
+            pointer1++;
+        }
+        if(height*width>maxArea)
+        maxArea=height*width;
+     }
+     return maxArea;
+    }
+}
